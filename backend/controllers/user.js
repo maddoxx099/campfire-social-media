47c2e5078e3028e7deb3a311fe9b110fec94205c
Maddoxx <ishmitsingh462.is@gmail.com>
2024-02-27 08:24:58 +0530

    search

---
+++ backend/controllers/user.js
@@ -0,0 +1,18 @@
+const Users = require("../schemas/user");
+
+const userCtrl={
+    searchUser: async (req,res)=> {
+        try {
+            const users = await Users.find({
+                name: { $regex: req.query.name },
+              })
+                .limit(10)
+                .select("name nametag");
+        
+              res.json({ users });
+        } catch(err){
+            return res.status(500).json({msg: err.message});
+        }
+    }
+}
+module.exports = userCtrl;
