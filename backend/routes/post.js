5414a37045d13b1f8965528bff967ae3f46bd628
Maddoxx <ishmitsingh462.is@gmail.com>
2024-02-27 08:04:21 +0530

    adding routes for post and user

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,2 +1,19 @@
-const router = require("express").Router;
-const auth = require
+const auth = require("../middleware/auth");
+const postCtrl = require("../controllers/post");
+
+router.route("/posts")
+  .post(auth, postCtrl.createPost)
+  .get(auth, postCtrl.getPosts);
+
+router.route("/post/:id")
+  .get(auth, postCtrl.getPost)
+  .delete(auth, postCtrl.deletePost);
+
+router.patch("/post/:id/like", auth, postCtrl.likePost);
+router.patch("/post/:id/unlike", auth, postCtrl.unLikePost);
+
+router.get("/user_posts/:id", auth, postCtrl.getUserPosts);
+
+router.get("/post_discover", auth, postCtrl.getPostDiscover);
+
+module.exports = router;
